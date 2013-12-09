37927c6fdcfbb5d82e2e9b5564a039fafeea93bb
Levi Balling <levi.balling@iconfitness.com>
2013-12-09 12:22:48 -0700

    Added description to the status.java
    really just testing the branching features of git.

--- fecp/src/main/java/com/ifit/sparky/fecp/interpreter/status/Status.java
+++ fecp/src/main/java/com/ifit/sparky/fecp/interpreter/status/Status.java
@@ -1,3 +1,11 @@
+/**
+ * Handles all the status SuperClass items
+ * @author Levi.Balling
+ * @date 12/9/2013
+ * @version 1
+ * This class will handle the status items, mainly anything dealing with the reply from the system.
+ */
+
 package com.ifit.sparky.fecp.interpreter.status;
 
 /**
